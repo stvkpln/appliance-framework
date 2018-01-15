9e547d30b58683c3e5163d8e9cd956f0b708e205
Steve Kaplan <steve@intolerable.net>
2018-01-15 10:40:05 -0800

    Typo fix
    -VMHost was mispelled in Confirm-BackingNetwork.... that is now fixed!

--- net.intolerable.appliances/internal/Confirm-BackingNetwork.ps1
+++ net.intolerable.appliances/internal/Confirm-BackingNetwork.ps1
@@ -15,7 +15,7 @@ Function Confirm-BackingNetwork {
     	Write-Progress -Activity $Activity -Status $Status -CurrentOperation "Checking vSphere Distributed Portgroups"
 
         Try{
-            Get-VirtualPortGroup -Name $Network -VMHsot $VMHost -ErrorAction Stop
+            Get-VirtualPortGroup -Name $Network -VMHost $VMHost -ErrorAction Stop
         }
         Catch{
     	    throw "Network name '$($network)' not found, check and confirm that this portgroup exists on the connected vCenter Server"
